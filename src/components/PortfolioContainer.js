46ea7381e6ac4234a35ceb8e7938031130d94100
Jcool221 <jcool2939@gmail.com>
2023-02-16 12:06:25 -0500

    added footer

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -3,19 +3,20 @@ import NavTabs from './NavTabs';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import Footer from './pages/Footer';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'About') {
-      return <About />;
+    if (currentPage === 'Contact') {
+      return <Contact />;
     }
     if (currentPage === 'Portfolio') {
       return <Portfolio />;
     }
-    return <Contact />;
+    return <About />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
@@ -26,6 +27,8 @@ export default function PortfolioContainer() {
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
+      {/* footer */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/pages/Footer.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import SocialMedia from './SocialMedia';
+
+export default function Footer() {
+  return (
+    <div className='navbar fixed-bottom navbar-expand-lg navbar-dark '>
+      {/* <div className='container-fluid'> */}
+        <SocialMedia />
+      {/* </div> */}
+    </div>
+  );
+}
