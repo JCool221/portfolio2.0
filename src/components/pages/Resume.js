b319d65bc727428b704ca7d3bd5a17dde4dc6b26
JCool221 <109809337+JCool221@users.noreply.github.com>
2023-04-16 16:19:19 -0400

    updated resume hosting

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import resume from '../assets/Resume.png'
+// import resume from '../assets/Resume.png'
 
 export default function Resume() {
   return (
     <div className='d-flex justify-content-center'>
-      <img className='img-fluid' alt='My Resume, link coming soon' src={resume}/>
+      {/* <img className='img-fluid' alt='My Resume, link coming soon' src={resume}/> */}
+      <iframe
+      title='My Resume'
+      src='https://docs.google.com/document/d/e/2PACX-1vRL4b1I2ywhaHEHUqHYUjAm00nzVfl4PKqOFpLSEpayQVwwUny7e7bXHdwzRrH25Q/pub'
+      height="1000"
+      width="100%"
+      />
     </div>
   );
 }
